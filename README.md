92f3c2f14b9c9af719dd48c6045cc7a9360f0fc3
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2025-04-22 02:13:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# KonomiTV-PastEPG
+KonomiTVから録画番組の情報を取得して番組表風に表示するツール。
+
+利用するには、KonomiTVがCORSフリーになっている、もしくはKonomiTVと同じオリジンにこのページを配置する必要がある。
+
+## For Developers
+```
+git clone https://github.com/yyya-nico/KonomiTV-PastEPG.git
+cd ./KonomiTV-PastEPG
+npm install
+```
+
+### 開発モードで起動
+```
+npm run dev
+```
+
+### build
+```
+npm run build
+```
---
+++ src/main.js
@@ -0,0 +1,95 @@
+import './style.css'
+
+// import data from './test.json' assert { type: 'json' };
+
+const value = '192.168.1.8';
+const [ipv4, port] = value.split(':');
+const konomiHost = `${ipv4.replaceAll('.', '-')}.local.konomi.tv:${port ?? 7000}`;
+const apiBaseUrl = `https://${konomiHost}/api/`;
+
+let total = Infinity;
+let programs = [];
+
+for (let index = 1; index <= total / 100; index++) {
+    const data = await fetch(`${apiBaseUrl}videos?page=${index}`).then(res => res.json());
+    if (data.total === 0) {
+        break;
+    }
+    total = data.total;
+    programs = [...programs, ...data.recorded_programs];
+}
+
+programs = programs.reverse();
+
+const channelsDiv = document.getElementById('channels');
+const timesDiv = document.getElementById('times');
+const scheduleDiv = document.getElementById('schedule');
+
+// Extract unique channels and sort by channel name
+const channels = [...new Set(programs.sort((a, b) => a.channel.remocon_id - b.channel.remocon_id).map(p => p.channel.name))];
+
+// Generate channel names (sticky header)
+channels.forEach(channel => {
+    const channelDiv = document.createElement('div');
+    channelDiv.className = 'channel-name';
+    channelDiv.textContent = channel;
+    channelsDiv.appendChild(channelDiv);
+});
+
+// Generate time labels
+const startTime = new Date(programs.reduce((a, b) => new Date(a.start_time) < new Date(b.start_time) ? a : b).start_time);
+const endTime = new Date(programs.reduce((a, b) => new Date(a.end_time) > new Date(b.end_time) ? a : b).end_time);
+const timeLabels = [];
+
+for (let time = new Date(startTime.setMinutes(0, 0, 0)); time <= endTime; time.setHours(time.getHours() + 1)) {
+    timeLabels.push(new Date(time));
+}
+
+timeLabels.forEach(time => {
+    const timeDiv = document.createElement('div');
+    timeDiv.className = 'time-label';
+    if (time.getHours() === 0) {
+        timeDiv.innerText = `${time.toLocaleString([], { month: 'numeric', day: 'numeric', weekday: 'short', hour: 'numeric' })}`;
+    } else {
+        timeDiv.textContent = time.toLocaleTimeString([], { hour: 'numeric' });
+    }
+    timesDiv.appendChild(timeDiv);
+});
+
+// Generate program cells
+programs.forEach(program => {
+    const channelIndex = channels.findIndex(channel => channel === program.channel.name);
+    const startTimeMinutesString = new Date(program.start_time).getMinutes().toString().padStart(2, '0');
+    const getClassName = (program) => {
+        return 'genre_' + (() => {
+            switch (program.genres[0]?.major) {
+                case 'スポーツ':
+                    return 'sports';
+                case 'ドラマ':
+                    return 'drama';
+                case '音楽':
+                    return 'music';
+                case '映画':
+                    return 'movie';
+                case 'アニメ・特撮':
+                    return 'anime';
+                default:
+                    return 'none';
+            }}
+        )();
+    };
+    const programAnchor = document.createElement('a');
+    programAnchor.className = 'program';
+    programAnchor.classList.add(getClassName(program));
+    programAnchor.style.gridColumn = channelIndex + 1;
+    programAnchor.style.gridRowStart = Math.floor((new Date(program.start_time) - startTime) / (60 * 1000)) + 1;
+    programAnchor.style.gridRowEnd = `span ${Math.ceil(program.duration / 60)}`;
+    programAnchor.href = `https://yyya-nico.co/konomitv/videos/watch/${program.id}`;
+    programAnchor.target = '_blank';
+    programAnchor.title = program.title;
+    programAnchor.innerHTML = `
+        <div class="start-time">${startTimeMinutesString}</div>
+        ${program.title}
+    `;
+    scheduleDiv.appendChild(programAnchor);
+});
